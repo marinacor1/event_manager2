75ea374b0ffb8596286618bb4b0b4727eacf528f
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 15:08:12 -0700

    track with index

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,15 +1,19 @@
 require 'csv'
 puts "EventManager Initialized!"
 
-# contents = CSV.open "event_attendees.csv"
-# puts contents
-
 lines = File.readlines "event_attendees.csv"
-lines.each do |line|
-  next if line == " ,RegDate,first_Name, last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+# lines.each do |line|
+#   next if line == " ,RegDate,first_Name, last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
+
+#track by index
+row_index = 0
+lines.each_with_index do |line, index|
+  next if index == 1
   columns = line.split(",")
   name = columns[2]
-  puts name
+  puts name 
 end
-
-#in order to access columns[0], which is the first, split on column
