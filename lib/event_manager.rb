6097fb431ca5a9db81e793563706d60b8c5df855
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 15:00:34 -0700

    split lines

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,8 +6,10 @@ puts "EventManager Initialized!"
 
 lines = File.readlines "event_attendees.csv"
 lines.each do |line|
-  # next if line == " ,RegDate,first_Name, last_Name,"
+  next if line == " ,RegDate,first_Name, last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
   columns = line.split(",")
+  name = columns[2]
+  puts name 
   puts columns
 end
 
