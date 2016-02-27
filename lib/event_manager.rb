f1942df44d8c72b4c7d1e25e23c7ddf9cc301f1c
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 15:11:54 -0700

    adding symbols with csv

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -18,8 +18,8 @@ puts "EventManager Initialized!"
 #   puts name
 # end
 
-contents = CSV.open 'event_attendees.csv', headers: true
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
 contents.each do |row|
-  name = row[2]
+  name = row[:first_name]
   puts name
 end
