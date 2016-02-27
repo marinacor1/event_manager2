0b47c0b3e68869b351a176da6df7c372b8121e3e
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 15:02:09 -0700

    puts name

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,8 +9,7 @@ lines.each do |line|
   next if line == " ,RegDate,first_Name, last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
   columns = line.split(",")
   name = columns[2]
-  puts name 
-  puts columns
+  puts name
 end
 
 #in order to access columns[0], which is the first, split on column
