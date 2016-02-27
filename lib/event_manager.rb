eebd0e8b3188344668d1b383c9f012292658c659
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 15:10:22 -0700

    implementing csv format

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,7 +1,7 @@
 require 'csv'
 puts "EventManager Initialized!"
 
-lines = File.readlines "event_attendees.csv"
+# lines = File.readlines "event_attendees.csv"
 # lines.each do |line|
 #   next if line == " ,RegDate,first_Name, last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
 #   columns = line.split(",")
@@ -10,10 +10,16 @@ lines = File.readlines "event_attendees.csv"
 # end
 
 #track by index
-row_index = 0
-lines.each_with_index do |line, index|
-  next if index == 1
-  columns = line.split(",")
-  name = columns[2]
-  puts name 
+# row_index = 0
+# lines.each_with_index do |line, index|
+#   next if index == 1
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
+
+contents = CSV.open 'event_attendees.csv', headers: true
+contents.each do |row|
+  name = row[2]
+  puts name
 end
