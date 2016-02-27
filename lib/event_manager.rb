8b1314edc335128db9dc1719a884d1f178110160
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 14:03:25 -0700

    read lines working

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,5 +2,10 @@ require 'csv'
 # require_relative '../lib/event_attendees.csv'
 puts "EventManager Initialized!"
 
-contents = CSV.open "event_attendees.csv"
-puts contents
+# contents = CSV.open "event_attendees.csv"
+# puts contents
+
+lines = File.readlines "event_attendees.csv"
+lines.each do |line|
+  puts line
+end
