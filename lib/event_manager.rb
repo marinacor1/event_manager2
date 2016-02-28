a543931bb3305abfd8a48411cbd4ee09bb5a0926
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 22:24:13 -0700

    letter working

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,41 @@
+require 'erb'
+require 'csv'
+require 'sunlight/congress'
 
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+  def legislators_by_zipcode(zipcode)
+    legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+
+    legislator_names = legislators.collect do |legislator|
+      "#{legislator.first_name} #{legislator.last_name}"
+      end
+      legislator_string = legislator_names.join(",")
+  end
+
+  puts "EventManager initialized."
+
+  contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+  template_letter = File.read "form_letter.html"
+
+  contents.each do |row|
+    name = row[:first_name]
+    zipcode = clean_zipcode(row[:zipcode])
+    legislators = legislators_by_zipcode(zipcode)
+
+    form_letter = erb_template.result(binding)
+    puts form_letter
+  end
+
+# meaning_of_life = 42
+# question = "The Answer to the Ultimate Question of Life, the Universe, and Everything is <%= meaning_of_life %>"
+# template = ERB.new question
+# results = template.result(binding)
+# puts results
 #   # lines = File.readlines "event_attendees.csv"
 #   # lines.each do |line|
 #   #   next if line == " ,RegDate,first_Name, last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
@@ -21,36 +58,7 @@
 # em.file_read
 # em.contents_organization
 
-require 'csv'
-require 'sunlight/congress'
 
-Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
-
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5,"0")[0..4]
-end
-
-
-  def legislators_by_zipcode(zipcode)
-    legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-    legislator_names = legislators.collect do |legislator|
-      "#{legislator.first_name} #{legislator.last_name}"
-      end
-      legislator_string = legislator_names.join(",")
-        # puts "#{name} #{zipcode} #{legislator_string}"
-  end
-
-  puts "EventManager initialized."
-
-  contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
-
-  contents.each do |row|
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    legislators = legislators_by_zipcode(zipcode)
-    puts "#{name} #{zipcode} #{legislators}"
-  end
 
       # legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
       #accepts a signle zipcode and returns a comma- separated string of legislator names
