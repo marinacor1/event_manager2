b6db186bf1207af6a2e8acd6eb514f294a8b7d50
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 20:57:19 -0700

    first legislators

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,40 +1,48 @@
-require 'pry'
+
+#   # lines = File.readlines "event_attendees.csv"
+#   # lines.each do |line|
+#   #   next if line == " ,RegDate,first_Name, last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
+#   #   columns = line.split(",")
+#   #   name = columns[2]
+#   #   puts name
+#   # end
+#
+#   #track by index
+#   # row_index = 0
+#   # lines.each_with_index do |line, index|
+#   #   next if index == 1
+#   #   columns = line.split(",")
+#   #   name = columns[2]
+#   #   puts name
+#   # end
+# end
+#
+# em = EventManager.new
+# em.file_read
+# em.contents_organization
+
 require 'csv'
 require 'sunlight/congress'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
-# lines = File.readlines "event_attendees.csv"
-# lines.each do |line|
-#   next if line == " ,RegDate,first_Name, last_Name,Email_Address,HomePhone,Street,City,State,Zipcode\n"
-#   columns = line.split(",")
-#   name = columns[2]
-#   puts name
-# end
-
-#track by index
-# row_index = 0
-# lines.each_with_index do |line, index|
-#   next if index == 1
-#   columns = line.split(",")
-#   name = columns[2]
-#   puts name
-# end
-class FileCheck
-  def file_read
-    contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
-    contents.each do |row|
-      name = row[:first_name]
-      zipcode = zipcode_check(row[:zipcode])
-      puts "#{name} #{zipcode}"
-    end
-  end
-
-  def zipcode_check(zipcode)
-    zipcode.to_s.rjust(5, "0")[0..4]
-  end
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
-puts "EventManager Initialized!"
 
-fc = FileCheck.new
-fc.file_read
+puts "EventManager initialized."
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+    legislator_names = []
+    legislators.each do |legislator|
+      legislator_name = "#{legislator.first_name} #{legislator.last_name}"
+      legislator_names.push(legislator_name)
+    end
+      puts "#{name} #{zipcode} #{legislator_names}"
+    
+    end
