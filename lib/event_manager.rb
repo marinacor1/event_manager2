acc413816396f9d871eeeaeee984b9151fa366f2
marinacor1 <marinaspam1234@gmail.com>
2016-02-27 22:51:01 -0700

    works but does not print full names

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,6 @@
-require 'erb'
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -9,27 +9,33 @@ def clean_zipcode(zipcode)
 end
 
   def legislators_by_zipcode(zipcode)
-    legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+    Sunlight::Congress::Legislator.by_zipcode(zipcode)
+  end
 
-    legislator_names = legislators.collect do |legislator|
-      "#{legislator.first_name} #{legislator.last_name}"
-      end
-      legislator_string = legislator_names.join(",")
+  def save_thank_you_letters(id,form_letter)
+    Dir.mkdir("output") unless Dir.exists? "output"
+    filename = "output/thanks_#{id}.html"
+
+    File.open(filename, 'w') do |file|
+      file.puts form_letter
+    end
   end
 
   puts "EventManager initialized."
-
   contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
-  template_letter = File.read "form_letter.html"
-  erb_template = ERB.new(template_letter)
-  contents.each do |row|
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    legislators = legislators_by_zipcode(zipcode)
 
-    form_letter = erb_template.result(binding)
-    puts form_letter
-  end
+    template_letter = File.read "form_letter.erb"
+    erb_template = ERB.new(template_letter)
+
+    contents.each do |row|
+      id = row[0]
+      name = row[:first_name]
+      zipcode = clean_zipcode(row[:zipcode])
+      legislators = legislators_by_zipcode(zipcode)
+      form_letter = erb_template.result(binding)
+
+      save_thank_you_letters(id,form_letter)
+    end
 
 # meaning_of_life = 42
 # question = "The Answer to the Ultimate Question of Life, the Universe, and Everything is <%= meaning_of_life %>"
