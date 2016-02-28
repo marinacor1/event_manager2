a799f1ffd47773afa45317c6b0247a6ba6e1bc8c
marinacor1 <marinaspam1234@gmail.com>
2016-02-28 11:00:21 -0700

    fixed phone issue

--- lib/phones.rb
+++ lib/phones.rb
@@ -1,1 +1,28 @@
+require 'pry'
+require 'csv'
 puts "EventManager initialized!"
+
+def clean_phones(phone)
+  if phone.length < 10
+    phone = 'bad'
+  elsif phone.length == 11
+    if phone[0] == '1'
+      phone = phone[1..-1]
+    else
+      phone = 'bad'
+    end
+  elsif phone.length > 10
+    phone = 'bad'
+  else
+    phone
+  end
+  phone
+end
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+contents.each do |row|
+  home_phone = row[:homephone]
+  home_phone.gsub!(/\D/, "")
+  phone = clean_phones(home_phone)
+  puts phone
+end
